2961093e1233f72292cbf96f0c1df947a658c5b1
Jacob Yaw <yawjacob@yahoo.com>
2022-02-23 17:35:24 -0600

    testing

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,28 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorData = directors.map(director => {
+    return (
+      <div key={director}>
+        <h3>{director.name}</h3>
+        <ul>
+          {director.movies.map(movie => {
+            return (
+              <li key={movie}>{movie}</li>
+            )
+          })}
+        </ul>
+      </div>
+
+    )
+  })
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorData}
+  </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,32 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieData = movies.map(movie => {
+    console.log(movie.genres)
+    return (
+    <div key={movie.title}>
+      <h3>Name: {movie.title}</h3>
+      <p>Time: {movie.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {
+        movie.genres.map(genre => {
+          return (
+            <li key={genre.id}>{genre}</li>
+          )
+        })
+        }
+      </ul>
+    </div>
+    )
+  })
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movieData}
+  </div>
+  )
 }
 
 export default Movies;
